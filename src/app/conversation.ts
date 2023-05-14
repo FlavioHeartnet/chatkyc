c45048d2bf2b709309bd096e0641c7f4403ab6d9
Flavio Nogueira <flavionogueirabarros@gmail.com>
2023-05-14 00:34:53 -0300

    fixing overflow and scroll wip

--- src/app/conversation.ts
+++ src/app/conversation.ts
@@ -3,4 +3,10 @@ import { chatStrings } from "./strings/strings";
 export const conversationList = [
     chatStrings.us.explanationText1,
     chatStrings.us.explanationText2,
+    chatStrings.us.explanationText2,
+    chatStrings.us.explanationText2,
+    chatStrings.us.explanationText2,
+    chatStrings.us.explanationText2,
+    chatStrings.us.explanationText2,
+    
 ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,19 @@ export default function Home() {
   let delay = 0
 
   return (
-    <main className="grid min-h-screen items-center place-content-center justify-center">
-      <div className='mt-20 lg:mx-60 sm: mx-1'>
+    <main className="grid fixed bottom-0 container mx-auto">
+      <div className=' overflow-y-scroll overflow-visible'>
         {conversationList.map(async (dialog, index) => {
           delay++
           return <MessageComponent key={index} string = {dialog} delay={delay} mainDelay={delay - 1} />
         })}
       </div>
       <form className='
-              w-full
-             fixed
+             relative
              inset-x-0
              bottom-0
              p-4'>
-        <input type='text' className='form-input px-4 py-3 rounded-lg w-full text-black' name='messageSend'/>
+        <textarea className='form-textarea h-12 px-4 py-3 rounded-lg w-1/2 text-black' name='messageSend'/>
         <button className='form-button'></button>
       </form>
     </main>
