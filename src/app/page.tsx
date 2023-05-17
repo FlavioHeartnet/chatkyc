912735a82db6be6211a38da954b8121687f3b99e
Flavio <flavionogueirabarros@gmail.com>
2023-05-17 18:46:50 -0300

    adjust text box and add box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,36 @@
 import MessageComponent from './components/message-component'
 import { conversationList } from './conversation' 
+import Image from 'next/image'
 export default function Home() {
   let delay = 0
 
   return (
-    <main className="grid fixed bottom-0 container mx-auto">
-      <div className=' overflow-y-scroll overflow-visible'>
+    <main className="grid fixed bottom-0">
+      <div className='mx-20 lg:mx-60 sm: mx-1 scroll-smooth'>
         {conversationList.map(async (dialog, index) => {
           delay++
           return <MessageComponent key={index} string = {dialog} delay={delay} mainDelay={delay - 1} />
         })}
-      </div>
-      <form className='
+        <div className='
+              flex
              relative
              inset-x-0
              bottom-0
              p-4'>
-        <textarea className='form-textarea h-12 px-4 py-3 rounded-lg w-1/2 text-black' name='messageSend'/>
-        <button className='form-button'></button>
-      </form>
+        <textarea className='flex-1 form-textarea h-12 px-4 py-3 rounded-l-lg w-full text-black border-none' name='messageSend'/>
+        <button className='
+          bg-lime-500
+          active:bg-lime-700 
+          hover:bg-lime-600  
+          w-20 
+          flex-none 
+          form-button 
+          rounded-r-lg 
+          text-white'>
+            <Image className='ml-6 invert' src={'/send.png'} alt={''} width={30} height={30}/>
+          </button>
+      </div>
+      </div>
     </main>
   )
 }
