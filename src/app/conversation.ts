37d367f21b2af8225e417a0339c5ba1877126172
Flavio Nogueira <flavionogueirabarros@gmail.com>
2023-06-03 20:38:12 -0300

    Adding new text in the conversation

--- src/app/conversation.ts
+++ src/app/conversation.ts
@@ -3,9 +3,5 @@ import { chatStrings } from "./strings/strings";
 export const conversationList = [
   chatStrings.us.explanationText1,
   chatStrings.us.explanationText2,
-  chatStrings.us.explanationText2,
-  chatStrings.us.explanationText2,
-  chatStrings.us.explanationText2,
-  chatStrings.us.explanationText2,
-  chatStrings.us.explanationText2,
+  chatStrings.us.explanationText3,
 ];
